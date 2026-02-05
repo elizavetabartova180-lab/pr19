1f51e81bd31ee7c5b5468ac86088e295e8294e96
ADMIN <ADMIN@MSI>
2026-02-06 04:13:29 +0500

    Доработанная элемент Item. Бартова

--- Elements/Item.xaml.cs
+++ Elements/Item.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.IO;
+using System;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace FurnitureStore_Bartova.Elements
 {
@@ -10,6 +13,13 @@ namespace FurnitureStore_Bartova.Elements
         public Item(Classes.Item item)
         {
             InitializeComponent();
+            if (item != null) {
+                if (File.Exists(Directory.GetCurrentDirectory() + "/" + item.src))
+                    image.Source = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "/" + item.src));
+                
+                price.Content = item.price;
+                name.Content = item.name;
+            }
         }
     }
 }
